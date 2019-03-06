4d310b9fc0ce02da3f5ae17f9f359b9c49b0f8c0
diurno <diurno@pc-aula>
2019-03-06 10:04:26 +0100

    4efcde584894598

--- operararrayj/src/operararray/Array.java
+++ operararrayj/src/operararray/Array.java
@@ -3,26 +3,32 @@ package operararray;
 public class Array {
 
 	public static void main(String[] args) {
-		rellenaArray();
-	}
-	
-	
-	public static void rellenaArray() {
-		int array [] = new int[10];
+		int array [] = new int[150];
 		
-		// Inicializaci�n del array
-		// Para asignar n�meros al azar entre un l�mite inferior y otro superior
-		// utilizaremos la siguiente f�rmula
-		//   n�meroAleatorio * (limiteSuperior - limiteInferior)  +  limiteInferior
-			for (int i = 0; i < array.length; i++) {
-				array[i] = (int) Math.round(Math.random() * (20-1)+1);
-				
+	
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) Math.round(Math.random() * (100 - (-100))) + (-100);
+		}
+		
+
+		System.out.println();
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		
+		
+		for (int i = 0; i < array.length; i++) {
+			if ((array[i] & 1) == 0) { 
+				array[i] = 0 - array[i];
 			}
-		// Impresi�n del array antes del cambio de signo
+		}
+		
+	
 		System.out.println();
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
 		
 	}
+	
 }
